041e5323992b74666457a3fba12017e43fd0fc4f
Vivek Kumar <102019065+vivekk-27@users.noreply.github.com>
2022-07-29 19:11:06 +0530

    Add files via upload

---
+++ Day4/Day4.js
@@ -0,0 +1,5 @@
+function change(){
+document.querySelector('.container').style.width=  "100%"
+document.querySelector('.container').style.borderRadius= "30px"
+document.querySelector('.content').innerText="width is 100% now";
+}
